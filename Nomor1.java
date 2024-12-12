e05eac09eb236a502ac259a8357124b0cd980b8f
Salmanridhwan <salmanridhwan04@gmail.com>
2024-12-12 09:18:00 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,6 +1,13 @@
 import java.util.Scanner;
 
 public class Nomor1 {
+    public static void tampilkanGanjil(int N) {
+        for (int i = 0; i <= N; i++) {
+            if (i % 2 != 0) {
+                System.out.print(i);
+            }
+        }
+    }
 
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
@@ -11,11 +18,5 @@ public class Nomor1 {
 
         System.out.println(Nomor1.class.getDeclaredMethods().length); 
     }
-        public static void tampilkanGanjil(int N) {
-        for (int i = 0; i <= N; i++) {
-            if (i % 2 != 0) {
-                System.out.print(i);
-            }
-        }
-    }
+
 }
